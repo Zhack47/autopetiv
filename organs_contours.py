8614f9cc5e3c05b41387f17a70717bf962b03269
PTB Admin <zacharia.mesbah@insa-rouen.fr>
2025-07-09 11:18:57 +0200

    typo

--- organs_contours.py
+++ organs_contours.py
@@ -2,7 +2,7 @@ if __name__ == "__main__":
     import os
     from os.path import join
     from totalsegmentator.python_api import totalsegmentator
-    root_path = "/mnt/disk_2/Zach/AutopetIV/"
+    root_path = "/mnt/disk_2/Zach/autopetIV/"
     names = [i.split(".nii.gz")[0] for i in sorted(os.listdir(join(root_path, "labelsTr")))]
     for name in names:
         print(name)
