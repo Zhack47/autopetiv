0671d447da5ec7919f6387cf28a55a676b146738
zhack47 <mesbah.zacharia@sfr.fr>
2025-07-15 14:42:46 +0200

    Fix: better pos/neg handling

--- heatmaps.py
+++ heatmaps.py
@@ -26,13 +26,20 @@ def save_heatmap(pet_path, tumor_clicks, bg_clicks, output_path):
     ref = pet_nii.get_fdata()
     ref_shape = ref.shape
     ref_affine = pet_nii.affine
-    point_map = np.zeros(ref_shape)
+    point_map_pos = np.zeros(ref_shape)
+    point_map_neg = np.zeros(ref_shape)
     print(ref_shape)
     for t_click in tumor_clicks:
-        point_map[tuple(t_click)]=1
+        print(t_click)
+        point_map_pos[(t_click[0],t_click[1],t_click[2])]=1
     for b_click in bg_clicks:
-        point_map[tuple(b_click)]=-1
-    heatmap = gaussian_filter(point_map, 6)
+        print(b_click)
+        point_map_neg[(b_click[0],b_click[1],b_click[2])]=1
+    heatmap_pos =  gaussian_filter(point_map_pos, 6)
+    heatmap_neg =  gaussian_filter(point_map_neg, 6)
+    heatmap_pos[ref<=2.5] = 0
+    heatmap_neg[heatmap_pos!=0] = 0
+    heatmap = heatmap_pos/np.max(heatmap_pos) - heatmap_neg/np.max(heatmap_neg)
     out = nib.Nifti1Image(heatmap, affine=ref_affine)
     nib.save(out, output_path)
 
@@ -42,5 +49,13 @@ images_root_path = "/mnt/disk_2/Zach/autopetIV/imagesTr"
 for json_path in sorted(os.listdir(json_root_path))[:4]:
     tumor_clicks, bg_clicks = get_coords(os.path.join(json_root_path, json_path))
     pet_path = os.path.join(images_root_path, json_path.replace("_clicks.json", "_0001.nii.gz"))
-    print(tumor_clicks)
-    save_heatmap(pet_path, tumor_clicks, bg_clicks, json_path.replace("_clicks.json", "_0002.nii.gz"))
+    save_heatmap(pet_path, tumor_clicks, bg_clicks, json_path.replace("_clicks.json", "_0002.nii.gz").replace("FDG_PSMA_PETCT_pre-simulated_clicks/", "/mnt/disk_2/Zach/Autopet_Heatmaps"))
+
+
+
+# For local testing
+'''pet_path = "fdg_e252be4334_07-09-2004-NA-PET-CT Ganzkoerper  primaer mit KM-45425_0001.nii.gz"
+json_path = "FDG_PSMA_PETCT_pre-simulated_clicks/fdg_e252be4334_07-09-2004-NA-PET-CT Ganzkoerper  primaer mit KM-45425_clicks.json"
+tumor_clicks, bg_clicks = get_coords(json_path)
+save_heatmap(pet_path, tumor_clicks, bg_clicks, json_path.replace("_clicks.json", "_0002.nii.gz").replace("FDG_PSMA_PETCT_pre-simulated_clicks/", ""))
+'''
