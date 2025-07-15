7704a6a9aa0bf8e5fc189344a2a166741b932d76
zhack47 <mesbah.zacharia@sfr.fr>
2025-07-15 11:12:05 +0200

    Debug: Add gaussian + use tuple instead of stars

--- heatmaps.py
+++ heatmaps.py
@@ -26,20 +26,21 @@ def save_heatmap(pet_path, tumor_clicks, bg_clicks, output_path):
     ref = pet_nii.get_fdata()
     ref_shape = ref.shape
     ref_affine = pet_nii.affine
-    heatmap = np.zeros(ref_shape)
+    point_map = np.zeros(ref_shape)
     print(ref_shape)
     for t_click in tumor_clicks:
-        heatmap[tuple(t_click)]=1
+        point_map[tuple(t_click)]=1
     for b_click in bg_clicks:
-        heatmap[tuple(b_click)]=-1
-    heatmap = gaussian_filter(heatmap, 6)
+        point_map[tuple(b_click)]=-1
+    heatmap = gaussian_filter(point_map, 6)
     out = nib.Nifti1Image(heatmap, affine=ref_affine)
     nib.save(out, output_path)
 
 
 json_root_path = "FDG_PSMA_PETCT_pre-simulated_clicks"
 images_root_path = "/mnt/disk_2/Zach/autopetIV/imagesTr"
-for json_path in os.listdir(json_root_path)[:4]:
+for json_path in sorted(os.listdir(json_root_path))[:4]:
     tumor_clicks, bg_clicks = get_coords(os.path.join(json_root_path, json_path))
     pet_path = os.path.join(images_root_path, json_path.replace("_clicks.json", "_0001.nii.gz"))
+    print(tumor_clicks)
     save_heatmap(pet_path, tumor_clicks, bg_clicks, json_path.replace("_clicks.json", "_0002.nii.gz"))
