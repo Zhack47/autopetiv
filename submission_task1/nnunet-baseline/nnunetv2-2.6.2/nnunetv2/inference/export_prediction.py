d2558b5262b27391ebc186a570571c43505abe9c
zhack47 <mesbah.zacharia@sfr.fr>
2025-07-29 14:11:22 +0200

    Debug

--- submission_task1/nnunet-baseline/nnunetv2-2.6.2/nnunetv2/inference/export_prediction.py
+++ submission_task1/nnunet-baseline/nnunetv2-2.6.2/nnunetv2/inference/export_prediction.py
@@ -28,9 +28,7 @@ def convert_predicted_logits_to_segmentation_with_correct_shape(predicted_logits
         len(properties_dict['shape_after_cropping_and_before_resampling']) else \
         [spacing_transposed[0], *configuration_manager.spacing]
     print(predicted_logits.shape)
-    lesion_predicted_logits = predicted_logits[:3]
-    print(lesion_predicted_logits.sum(0))
-    lesion_predicted_logits[2], _ = predicted_logits[2:].max(0)
+    lesion_predicted_logits = torch.cat([predicted_logits[:2], predicted_logits[2:].max(0)[0]], 0)
     print(lesion_predicted_logits.sum(0))
     print(lesion_predicted_logits.shape)
     lesion_predicted_logits = configuration_manager.resampling_fn_probabilities(lesion_predicted_logits,
