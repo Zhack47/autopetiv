af2c4919687c4e22175b9f8d00d286b9526a68b6
zhack47 <mesbah.zacharia@sfr.fr>
2025-07-30 11:56:57 +0200

    Fix: weights loading

--- submission_task1/nnunet-baseline/nnunetv2-2.6.2/nnunetv2/run/load_pretrained_weights.py
+++ submission_task1/nnunet-baseline/nnunetv2-2.6.2/nnunetv2/run/load_pretrained_weights.py
@@ -20,7 +20,7 @@ def load_pretrained_weights(network, fname, verbose=False):
     if dist.is_initialized():
         saved_model = torch.load(fname, map_location=torch.device('cuda', dist.get_rank()))
     else:
-        saved_model = torch.load(fname)
+        saved_model = torch.load(fname, weights_only=False)
     pretrained_dict = saved_model['network_weights']
 
     skip_strings_in_pretrained = [
