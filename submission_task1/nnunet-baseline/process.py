5644ce00e5dcc51942a0edcd59f49db35260ee34
zhack47 <mesbah.zacharia@sfr.fr>
2025-08-01 09:40:46 +0200

    Debug: trying smaller window

--- submission_task1/nnunet-baseline/process.py
+++ submission_task1/nnunet-baseline/process.py
@@ -147,7 +147,7 @@ class Autopet_baseline:
 
         print("Creating", end="")
         predictor = nnUNetPredictor(
-            tile_step_size=0.6,
+            tile_step_size=0.4,
             use_mirroring=True,
             verbose=False,
             verbose_preprocessing=False,
